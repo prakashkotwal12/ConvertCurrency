8db34ea1f6cdd74c15b00a387ae2208f6da8cd97
Prakash <prakash.kotwal@payrix.com>
2024-02-11 10:21:12 +0545

    Initial commit

---
+++ ConvertCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertCurrency
+//
+//  Created by Prakash Kotwal on 11/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		print("convert cureency")
+	}
+
+
+}
+
